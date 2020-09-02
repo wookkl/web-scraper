14a3a59d3e0a03aa727e4e7112596c302930011d
wookkl <57362359+wookkl@users.noreply.github.com>
2020-09-03 01:02:29 +0900

    get Pages

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
+
+
 indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
-print(indeed_result.text)
+indeed_soup  = BeautifulSoup(indeed_result.text, "html_parser")
+pagination = indeed_soup.find("div",{"class": "pagination"})
+pages = pagination.find_all('a')
+print(pages)
