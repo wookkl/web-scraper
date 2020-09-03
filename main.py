6937134ad3e6036b7a5ce89a50398370e3de271c
wookkl <57362359+wookkl@users.noreply.github.com>
2020-09-04 02:09:22 +0900

    Extracting Companies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from indeed import extract_indeed_pages, extract_indeed_jobs
 
 last_indeed_page = extract_indeed_pages()
-print(extract_indeed_jobs(last_indeed_page))
+extract_indeed_jobs(last_indeed_page)
