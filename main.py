1d4e7302a1db232b366f9fcfbddc72210e72b432
wookkl <57362359+wookkl@users.noreply.github.com>
2020-09-04 00:13:25 +0900

    Request Each Page

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
-indeed_soup  = BeautifulSoup(indeed_result.text, 'html.parser')
-pagination = indeed_soup.find("div",{"class": "pagination"})
-links = pagination.find_all('a')
-pages=[]
-for link in links[:-1]:
-   pages.append(int(link.text))
-max_page = pages[-1]
-for n in range(max_page):
-    print(f"start={n*50}")
+last_indeed_page = extract_indeed_pages()
+extract_indeed_jobs(last_indeed_page)
